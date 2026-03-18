e1b38b7f2a4022d86ea4fa1a262cb5f53f752354
Ymekki <yasin@mekki.net>
2026-03-17 23:29:59 -0500

    Run Playwright traditional test in headless mode for CI

--- src/test/java/playwrightTraditional/BookstoreTraditionalTest.java
+++ src/test/java/playwrightTraditional/BookstoreTraditionalTest.java
@@ -22,7 +22,7 @@ public class BookstoreTraditionalTest {
     static void setUp() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(true)
         );
     }
 
